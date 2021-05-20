dd723bbe1b1a65e4b79b6be9d4ea7bf327f59fb8
Furvent <furventt@gmail.com>
2021-05-20 16:54:33 +0200

    First commit Maven config + spring boot initializer

---
+++ src/test/java/fr/eql/ticketting/TickettingApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eql.ticketting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

a53bcbb300ca9dad814ce2c5d3c85d7a3002cdab
Roch-Jacquot <roch.jacquot@gmail.com>
2021-05-25 11:52:57 +0200

    ajoute formulaire inscription basique et fonctions dédiées dans UserController

--- src/main/java/fr/eql/ticketting/controller/UserController.java
+++ src/main/java/fr/eql/ticketting/controller/UserController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 import fr.eql.ticketting.entity.User;
 import fr.eql.ticketting.service.UserService;
@@ -24,5 +27,18 @@ public class UserController {
 		model.addAttribute("users", users);
 		return "usersDebug"; // le nom du template
 	}
+	
+	@GetMapping("signup")
+	String displaySignUpPage(Model model) {
+		User user = new User();
+		model.addAttribute("user", user);
+		return "signUp";
+	}
+	@PostMapping("/list-users")
+	public RedirectView signUpNewUser(@ModelAttribute("user") User user) {
+		service.save(user);
+		return new RedirectView("/list-users");
+	}
+	
 
 }

e827472e464e2e2d294a314e668d2988853c6f46
Raphael <louvet.raphael@gmail.com>
2021-05-27 19:15:20 +0200

    Ajout deconnexion

--- src/main/java/fr/eql/ticketting/controller/ConnectionControler.java
+++ src/main/java/fr/eql/ticketting/controller/ConnectionControler.java
@@ -1,10 +1,13 @@
 package fr.eql.ticketting.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -44,5 +47,19 @@ public class ConnectionControler {
 			retour = new RedirectView("/connect");
 		}
 		return retour;
+	}
+	
+	/* version modele didi
+	@RequestMapping("/session-end")
+    public String finSession(Model model,HttpSession session) {
+	    session.invalidate();
+        return "/connect"; 
+    }*/
+	
+	@PostMapping("/deconnexion")
+	public RedirectView disconnect(@ModelAttribute("user") User user, Model model) {
+		user = new User();
+		model.addAttribute(user); 
+		return new RedirectView("/connect");
 	}
 }

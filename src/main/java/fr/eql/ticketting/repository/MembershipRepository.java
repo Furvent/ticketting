5b1242a2f9efef5b5cc556d2e88a896352eafc38
Mohamed Ahmed Hassine <m.ahmed.hassine@gmail.com>
2021-05-27 11:45:20 +0200

    Add getusersByGroup's feature

--- src/main/java/fr/eql/ticketting/repository/MembershipRepository.java
+++ src/main/java/fr/eql/ticketting/repository/MembershipRepository.java
@@ -3,9 +3,12 @@ package fr.eql.ticketting.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import fr.eql.ticketting.entity.Group;
 import fr.eql.ticketting.entity.Membership;
 import fr.eql.ticketting.entity.User;
 
 public interface MembershipRepository extends JpaRepository<Membership, Long>{
 	public List<Membership> findByUser(User user);
+	public List<Membership> findByGroup(Group group);
 }
